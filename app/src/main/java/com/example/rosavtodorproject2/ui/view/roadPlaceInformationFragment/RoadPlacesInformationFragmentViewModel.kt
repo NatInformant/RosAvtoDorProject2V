b4263c23afc5e633e72888003f065f129e434429
informant <artemiy.solodkin@mail.ru>
2024-05-15 23:42:07 +0500

    fourth fragment ui work start

---
+++ app/src/main/java/com/example/rosavtodorproject2/ui/view/roadPlaceInformationFragment/RoadPlacesInformationFragmentViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.rosavtodorproject2.ui.view.roadPlaceInformationFragment
+
+import androidx.lifecycle.ViewModel
+
+class RoadPlacesInformationFragmentViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
