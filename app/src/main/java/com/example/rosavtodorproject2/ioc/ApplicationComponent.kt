f41af8165f9495c64644e0ef75e60c4643ee79a3
informant <artemiy.solodkin@mail.ru>
2024-06-10 18:08:26 +0500

    refactoring continuex8 Di fixing and upgrading

--- app/src/main/java/com/example/rosavtodorproject2/ioc/ApplicationComponent.kt
+++ app/src/main/java/com/example/rosavtodorproject2/ioc/ApplicationComponent.kt
@@ -2,15 +2,10 @@ package com.example.rosavtodorproject2.ioc
 
 import com.example.rosavtodorproject2.BuildConfig
 import com.example.rosavtodorproject2.data.dataSource.AdvertisementsApi
-import com.example.rosavtodorproject2.data.dataSource.AdvertisementsRemoteDataSource
 import com.example.rosavtodorproject2.data.dataSource.MapPointsApi
-import com.example.rosavtodorproject2.data.dataSource.MapRemoteDataSource
 import com.example.rosavtodorproject2.data.dataSource.RoadAdvertisementsApi
-import com.example.rosavtodorproject2.data.dataSource.RoadAdvertisementsRemoteDataSource
 import com.example.rosavtodorproject2.data.dataSource.RoadPlacesApi
-import com.example.rosavtodorproject2.data.dataSource.RoadPlacesRemoteDataSource
 import com.example.rosavtodorproject2.data.dataSource.RoadsApi
-import com.example.rosavtodorproject2.data.dataSource.RoadsRemoteDataSource
 import com.example.rosavtodorproject2.domain.useCases.AdvertisementsUseCase
 import com.example.rosavtodorproject2.domain.useCases.MapPointsUseCase
 import com.example.rosavtodorproject2.domain.useCases.RoadAdvertisementsUseCase
@@ -24,7 +19,7 @@ import javax.inject.Scope
 @Scope
 annotation class AppComponentScope
 
-@dagger.Component(modules = [DataModule::class, MainViewModelModule::class, RoadsViewModelModule::class, RoadsInformationViewModelModule::class, RoadPlacesInformationViewModelModule::class, InteractiveMapViewModelModule::class])
+@dagger.Component(modules = [NetworkModule::class, ViewModelsModule::class])
 @AppComponentScope
 interface ApplicationComponent {
     fun getMainViewModelFactory(): MainViewModelFactory
@@ -32,11 +27,10 @@ interface ApplicationComponent {
     fun getRoadsViewModelFactory(): RoadsViewModelFactory
     fun getRoadInformationViewModelFactory(): RoadInformationViewModelFactory
     fun getRoadPlacesInformationViewModelFactory(): RoadPlacesInformationViewModelFactory
-    /*  fun getConversationViewModelFactory(): ConversationViewModelFactory*/
 }
 
 @dagger.Module
-object DataModule {
+object NetworkModule {
     @Provides
     @AppComponentScope
     fun getAdvertisementsApi(): AdvertisementsApi = Retrofit.Builder()
@@ -79,7 +73,7 @@ object DataModule {
 }
 
 @dagger.Module
-object MainViewModelModule {
+object ViewModelsModule {
     @Provides
     @AppComponentScope
     fun getMainViewModelFactory(
@@ -87,10 +81,6 @@ object MainViewModelModule {
     ): MainViewModelFactory {
         return MainViewModelFactory(advertisementsUseCase)
     }
-}
-
-@dagger.Module
-object RoadsViewModelModule {
     @Provides
     @AppComponentScope
     fun getRoadsViewModelFactory(
@@ -98,10 +88,6 @@ object RoadsViewModelModule {
     ): RoadsViewModelFactory {
         return RoadsViewModelFactory(roadsUseCase)
     }
-}
-
-@dagger.Module
-object RoadsInformationViewModelModule {
     @Provides
     @AppComponentScope
     fun getRoadInformationViewModelFactory(
@@ -109,10 +95,6 @@ object RoadsInformationViewModelModule {
     ): RoadInformationViewModelFactory {
         return RoadInformationViewModelFactory(roadAdvertisementsUseCase)
     }
-}
-
-@dagger.Module
-object RoadPlacesInformationViewModelModule {
     @Provides
     @AppComponentScope
     fun getRoadPlacesInformationViewModelFactory(
@@ -120,10 +102,6 @@ object RoadPlacesInformationViewModelModule {
     ): RoadPlacesInformationViewModelFactory {
         return RoadPlacesInformationViewModelFactory(roadPlacesUseCase)
     }
-}
-
-@dagger.Module
-object InteractiveMapViewModelModule {
     @Provides
     @AppComponentScope
     fun getInteractiveMapViewModelFactory(
