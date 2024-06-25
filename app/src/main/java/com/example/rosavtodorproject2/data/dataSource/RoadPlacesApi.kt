70a11b975aeae1da38d60f6e31fbe795316e404f
informant <artemiy.solodkin@mail.ru>
2024-06-25 18:58:00 +0500

    lfx more

--- app/src/main/java/com/example/rosavtodorproject2/data/dataSource/RoadPlacesApi.kt
+++ app/src/main/java/com/example/rosavtodorproject2/data/dataSource/RoadPlacesApi.kt
@@ -7,10 +7,10 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RoadPlacesApi {
-    @GET("roads/{roadName}/verifiedPoints/{type}")
+    @GET("roads/{roadName}/verifiedPoints/{pointType}")
     suspend fun getRoadPlaces(
         @Path("roadName") roadName: String,
-        @Path("type") type: String,
+        @Path("pointType") type: String,
         @Query("Coordinates.Latitude") latitude:Double,
         @Query("Coordinates.Longitude") longitude:Double,
         @Query("PointsCount") pointsCount:Int = 10,
